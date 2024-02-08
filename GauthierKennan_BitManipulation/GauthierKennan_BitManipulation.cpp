af012f3d9b7bbdc4f5c9dfee1ecf4b0449d16b65
Kennan Gauthier <KDGauthier@student.fullsail.edu>
2024-02-08 00:46:16 -0500

    Program is up and running. Pending revision.

--- GauthierKennan_BitManipulation/GauthierKennan_BitManipulation.cpp
+++ GauthierKennan_BitManipulation/GauthierKennan_BitManipulation.cpp
@@ -23,7 +23,9 @@ void LeftShift(unsigned int numb); // Left shifts the numb int parameter by 1.
 
 void RightShift(unsigned int numb); // Right shifts the numb int parameter by 1.
 
-int AskForNumber(); // Mehtod used to verify and store user input.
+int AskForOperation(); // Method used to verify and store user input for which bitwise operation they'd like to perform.
+
+int AskForNumber(); // Method used to store a number entered by the user and convert it into an int data type.
 
 
 
@@ -34,56 +36,110 @@ int main()
     std::cout << "The value of my global bit field is: " << bitField << std::endl;
     std::cout << "And it's value in binary is: " << ConvertToBinary(bitField) << "\n\n";
 
-
-
-    std::cout << "****************************************************************************\n\n";
-    std::cout << "Which operation would you like to perform?\n\n";
-
-
     bool condition = true; // exit condition for do while loop.
 
 
     // Vector of strings used to prompt user which bitwise operation they want to perform on the bitField unsinged int.
-    std::vector<std::string> bitwiseOperations = { "1 = TurnOff", "2 = TurnOff", "3 = Toggle", "4 = Negate", "5 = LeftShift", "6 = RightShift", "7 = Exit Program"};
+    std::vector<std::string> bitwiseOperations = { "1 = TurnOn", "2 = TurnOff", "3 = Toggle", "4 = Negate", "5 = LeftShift", "6 = RightShift", "7 = Exit Program"};
 
-    
-    for (std::string bitwise : bitwiseOperations) 
+
+    do
     {
-        std::cout << bitwise << std::endl; 
-    }
+        std::cout << "The value of my global bit field is: " << bitField << std::endl; 
+        std::cout << "And it's value in binary is: " << ConvertToBinary(bitField) << "\n\n";
 
-    int switchCase = AskForNumber();
-
-    std::cout << "\n\n" << switchCase << std::endl; 
-    
-    // switch(userInput)
+        std::cout << "****************************************************************************\n\n";  
+        std::cout << "Which operation would you like to perform?\n\n"; 
 
 
+        for (std::string bitwise : bitwiseOperations) // Prints out each string within the bitwiseOperations parameter. 
+        {
+            std::cout << bitwise << std::endl;  
+        }
 
 
-    //do
-    //{
-    //    std::cout << "****************************************************************************\n\n"; 
-    //    std::cout << "Which operation would you like to perform?\n\n";
+        int switchCase = AskForOperation(); // Determines which bitwise operation is performed.
 
 
 
-    //    for (std::string bitwise : bitwiseOperations) 
-    //    {
-    //        std::cout << bitwise << std::endl; 
-    //    }
+        switch (switchCase)
+        {
+        case 1: // Performs TurnOn() function on the bitfield.
+        {
+            int numbEntered = AskForNumber(); // Asks the user for the bit index they want to change.
+
+            TurnOn(numbEntered);
+
+            std::string newBinary = ConvertToBinary(bitField);
+
+            std::cout << "The bitField has been changed to:  " << newBinary << "\n\n"; 
+
+            break;
+        }
+        case 2: // Performs TurnOff() function on the bitfield.
+        {
+            int numbEntered = AskForNumber(); // Asks the user for the bit index they want to change.
+
+            TurnOff(numbEntered);
+
+            std::string newBinary = ConvertToBinary(bitField);
+
+            std::cout << "The bitField has been changed to:  " << newBinary << "\n\n"; 
+
+            break;
+        }
+        case 3: // Performs Toggle() function on the bitfield.
+        {
+            int numbEntered = AskForNumber(); // Asks the user for the bit index they want to change.
+
+            Toggle(numbEntered);
+
+            std::string newBinary = ConvertToBinary(bitField);
+
+            std::cout << "The bitField has been changed to:  " << newBinary << "\n\n"; 
+
+            break;
+        }
+        case 4: // Performs Negate() function on the bitfield.
+        {
+            Negate(bitField);
+
+            std::string newBinary = ConvertToBinary(bitField);
+
+            std::cout << "The bitField has been changed to:  " << newBinary << "\n\n"; 
+
+            break;
+        }
+        case 5: // Performs LeftShift() function on the bitfield.
+        {
+            LeftShift(bitField); 
+
+            std::string newBinary = ConvertToBinary(bitField); 
+
+            std::cout << "The bitField has been changed to:  " << newBinary << "\n\n"; 
+
+            break;
+        }
+        case 6: // Performs RightShift() function on the bitfield.
+        {
+            RightShift(bitField); 
+
+            std::string newBinary = ConvertToBinary(bitField); 
+
+            std::cout << "The bitField has been changed to:  " << newBinary << "\n\n"; 
+
+            break;
+        }
+        case 7: // Exits the program.
+        {
+            condition = false; 
+            break;
+        }
+
+        }
 
 
-    //    // Prompts user for input and stores it in a string variable.
-    //    std::cout << "\nPlease enter a number 1-7 here: ";
-    //    std::string userInput;
-    //    std::cin >> userInput;
-    //    std::cin.clear();
-    //    std::cin.ignore(INT_MAX, '\n');
-
-
-
-    //} while (condition);
+    } while (condition);
 
 
 
@@ -148,14 +204,15 @@ void RightShift(unsigned int numb)
     bitField = numb >> 1; 
 }
 
-int AskForNumber()
+int AskForOperation()
 {
     bool condition = true; // exit condition for the do while loop
 
+    // Prompts user for input and stores it in a string variable.
+    std::cout << "\nPlease enter a number 1-7 here: "; 
+
     do
-    {
-        // Prompts user for input and stores it in a string variable.
-        std::cout << "\nPlease enter a number 1-7 here: "; 
+    { 
         std::string userInput;
         std::cin >> userInput;
         std::cin.clear();
@@ -163,24 +220,64 @@ int AskForNumber()
 
         try
         {
-            int switchCase = std::stoi(userInput); 
+            int switchCase = std::stoi(userInput); // Converts userInput into an int data type
 
-            if (switchCase >= 1 && switchCase <= 7)
+            if (switchCase >= 1 && switchCase <= 7) // Checks if the number entered is between 1-7.
             {
                 return switchCase;
             }
-            else
+            else // If the number entered does not fall within the appropriate range (1-7).
             {
-                std::cout << "\n\nInvalid response. Please try again.\n\n";
+                std::cout << "\n\nInvalid response. Please try again: ";
             }
         }
-        catch (...)
+        catch (...) // If an error occurs converting the userInput from string to an int data type.
         {
-            std::cout << "\n\nInvalid response. Please try again.\n\n"; 
+            std::cout << "\n\nInvalid response. Please try again: "; 
         }
 
 
     } while (condition);
+}
+
+int AskForNumber()
+{
+    bool condition = true; // exit condition for the do while loop
+
+    // Prompts user for input and stores it in a string variable.
+    std::cout << "\nPlease enter a number 0-31 here: ";
+
+    do
+    {
+        std::string userInput;
+        std::cin >> userInput;
+        std::cin.clear();
+        std::cin.ignore(INT_MAX, '\n');
+
+        try
+        {
+            int bitIndex = std::stoi(userInput); // Converts userInput into an int data type
+
+            if (bitIndex >= 0 && bitIndex <= 31) // Checks if the number entered is between 0-31.
+            {
+                return bitIndex;
+            }
+            else // If the number entered does not fall within the appropriate range (0-31).
+            {
+                std::cout << "\n\nInvalid response. Please try again: ";
+            }
+        }
+        catch (...) // If an error occurs converting the userInput from string to an int data type.
+        {
+            std::cout << "\n\nInvalid response. Please try again: ";
+        }
+
+
+    } while (condition);
+    
+    
+    
+    return 0;
 }
 
 
