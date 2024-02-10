dc7beb243e197867023bf439c9884d38698ce550
Kennan Gauthier <KDGauthier@student.fullsail.edu>
2024-02-10 18:07:01 -0500

    Finished project.

--- GauthierKennan_Pointers/GauthierKennan_Pointers/GauthierKennan_Pointers.cpp
+++ GauthierKennan_Pointers/GauthierKennan_Pointers/GauthierKennan_Pointers.cpp
@@ -36,20 +36,35 @@ public:
 
 void GetCarInfo(Car& car); // Asks for user input to construct a car object. 
 
+void RepaintCar(Car* ptrCar, EnumColorDefinition newcolor); // Method used to change the mColor member field of a Car object.
+
+void AddMileage(Car* ptrCar, int milesToAdd); // Method used to increase the mMileage member field of a Car object.
+
+void PrintCar(Car c); // Method used to print out information about the car object using pass by value.
+void PrintCarPointer(Car* ptrCar); // Method used to print out information about the car object using pass by pointer.
+
+// Method used to convert EnumColorDefinition value (an int) into a string representing the intended color.
+std::string ShowCarColor(int colorIndex); 
+
+
+
 // Vector of strings to display color options to the user. To be used in the GetCarInfo() method.
 std::vector<std::string> colorOptions = { "0 = Red", "1 = Blue", "2 = Silver", "3 = White", "4 = Black", "5 = Orange" };
+
+// Vector of strings to show the user they can change the color or add mileage to a car object.
+std::vector<std::string> changeColorOrMileage = { "1 = Repaint car", "2 = Add mileage to car" };
+
+// Shows the user what cars they can change.
+std::vector<std::string> carSelection = { "0 = First Car", "1 = Second Car", "2 = Third Car", "3 = Exit program"};
 
 
 int main()
 {
     srand((unsigned)time(NULL)); // Provides seed value.
-    
 
     std::cout << "\n\n***************************\tProgram 1\t***************************\n\n";
-
-
-    // First array of 15 random numbers.
-    int numbArray[15];
+    
+    int numbArray[15]; // First array of 15 random numbers.
 
     for (int i = 0; i < 15; i++) // Adds a random number to each index within numbArray.
     {
@@ -83,7 +98,7 @@ int main()
 
 
     std::cout << "\n\n***************************\tProgram 3\t***************************\n\n";
-    std::cout << "This program will ask you to create an array of 3 car objects.\n";
+    std::cout << "This program will ask you to create an array of 3 car objects and modify them.\n";
 
     Car arrayOfCars[3]; // An array of three car objects.
 
@@ -93,63 +108,225 @@ int main()
     {
         std::cout << "\nFor Car " << iterator << ":\n\n";
         
-        GetCarInfo(arrayOfCars[i]);
+        GetCarInfo(arrayOfCars[i]); // Prompts the user for what 3 cars they want to build.
 
-        iterator++;
+        iterator++; // Increases the count of the iterator variable by 1.
     }
 
-    int iterator2 = 1; // Keeps track of which car is being displayed.
 
     std::cout << "\n\n***************************\tVehicle information\t***************************\n\n";
 
-    for (Car& car : arrayOfCars)
+    int iterator2 = 1; // Keeps track of which car is being displayed.
+    
+    std::cout << "Printing cars using pass by value:\n\n";
+    
+    for (Car& car : arrayOfCars) // Prints each car using PrintCar().
     {
-        std::string carColor;
-        switch (car.mColor)
-        {
-        case 0:
-        {
-            carColor = "Red";
-            break;
-        }
-        case 1:
-        {
-            carColor = "Blue";
-            break;
-        }
-        case 2:
-        {
-            carColor = "Silver";
-            break;
-        }
-        case 3:
-        {
-            carColor = "White";
-            break;
-        }
-        case 4:
-        {
-            carColor = "Black";
-            break;
-        }
-        case 5:
-        {
-            carColor = "Orange";
-            break;
-        }
-        }
-        
-        std::cout << "Car " << iterator2 << ":\t";
-        std::cout << car.mYear << " ";
-        std::cout << car.mColor << " ";
-        std::cout << car.mMake << " ";
-        std::cout << car.mModel << " with ";
-        std::cout << car.mMileage << " miles";
-        std::cout << "\n\n************************\n\n";
+        std::cout << "Car " << iterator2 << ":\t"; 
+        PrintCar(car); 
+        iterator2++; // Increases the count of the iterator2 variable by 1. 
+    }
+    
+    int iterator3 = 1; // Keeps track of which car is being displayed.
+
+    std::cout << "\n\nPrinting cars using pass by pointer:\n\n";
+
+    for (Car& car : arrayOfCars) // Prints each car using PrintCarPointer().
+    {
+        std::cout << "Car " << iterator3 << ":\t";
+        PrintCarPointer(&car); 
+        iterator3++; // Increases the count of the iterator2 variable by 1.
     }
 
-}
+    
 
+    std::cout << "\n\n***************************\tChange color or Add mileage\t***************************\n\n";
+
+    bool exitProgram = true; // exit condition for do while loop.
+
+    do
+    {
+        int iterator4 = 1; // Keeps track of which car is being displayed. 
+        int carIndex; // Stores user's choice for which car to change.
+        int change; // Stores user's choice to change the car's color or add miles.
+        int selectedColor; // New color selected by the user
+        int addedMiles; // Amount of miles to add to the mMileage member field chosen by the user.
+        EnumColorDefinition newColor; // selectedColor casted as an enum for the EnumColorDefinition.
+
+        // Exit conditions for their respective do while loops.
+        bool selectCarToChangeCondition = true; 
+        bool selectColorOrMilesCondition = true; 
+        bool selectColorCondition = true; 
+        bool changeMilesCondition = true;
+        
+        for (Car& car : arrayOfCars) // Prints each car using PrintCarPointer().
+        {
+            std::cout << "Car " << iterator4 << ":\t";
+            PrintCarPointer(&car);
+            iterator4++; // Increases the count of the iterator2 variable by 1.
+        }
+
+
+        for (std::string carOption : carSelection) // Prints out which car the user would like to change.
+        {
+            std::cout << "\t" << carOption; 
+        }
+        std::cout << "\n\n"; 
+
+        // Asks the user which car they would like to change.
+        do
+        {
+            std::cout << "Enter either 0, 1, or 2 for which car would you like to change (Enter 3 to exit the program): ";
+            std::string selection; 
+            std::cin >> selection; 
+            try
+            {
+                carIndex = std::stoi(selection);  
+                if (carIndex == 0 || carIndex == 1 || carIndex == 2) 
+                {
+                    selectCarToChangeCondition = false; 
+                }
+                else if (carIndex == 3)
+                {
+                    selectCarToChangeCondition = false;
+                    exitProgram = false;
+                }
+                else
+                {
+                    std::cout << "Error. Please enter 0, 1, or 2.\n";
+                }
+
+            }
+            catch (...)
+            {
+                std::cout << "Error. Please enter 0, 1, or 2.\n";
+            }
+
+        } while (selectCarToChangeCondition);
+        ClearInput(); 
+        std::cout << "\n"; 
+
+        if (carIndex == 3) // If the user decides to exit the program.
+        {
+            continue; // Skips over the rest of the 'exitProgram' do while loop.
+        }
+
+
+        for (std::string option : changeColorOrMileage) // Prints out 2 options to change a car's color or add miles.
+        {
+            std::cout << "\t" << option << "\t";
+        }
+        std::cout << "\n\n";
+
+
+        // Asks the user if they want to change the car's color or add mileage to the vehicle.
+        do
+        {
+            std::cout << "Please enter 1 or 2 to change the color or add miles to a car: ";
+            std::string selection;
+            std::cin >> selection;
+            try
+            {
+                change = std::stoi(selection);
+                if (change == 1 || change == 2)
+                {
+                    selectColorOrMilesCondition = false;
+                }
+                else
+                {
+                    std::cout << "Error. Please enter 1 or 2.\n";
+                }
+
+            }
+            catch (...)
+            {
+                std::cout << "Error. Please enter 1 or 2.\n";
+            }
+        } while (selectColorOrMilesCondition);
+        ClearInput();
+        std::cout << "\n";
+
+
+        if (change == 1) // If the user chose to change a car's color
+        {
+            for (std::string colorOption : colorOptions) 
+            {
+                std::cout << "     " << colorOption; 
+            }
+            std::cout << "\n\n";
+            
+            // Asks the user what color they'd like to change the color to.
+            do
+            {
+                std::cout << "\nEnter a number 0-5 to select a color: "; 
+                std::string input; 
+                std::cin >> input; 
+                try
+                {
+                    int selectedColor = std::stoi(input); 
+                    if (selectedColor >= 0 && selectedColor <= 5) // Checks if the number entered is between 0-5. 
+                    {
+                        newColor = (EnumColorDefinition)selectedColor; 
+                        selectColorCondition = false; 
+                    }
+                    else
+                    {
+                        std::cout << "\nError. Please enter a valid number 0-5.";
+                    }
+                }
+                catch (...) // If the string cannot be converted into an int.
+                {
+                    std::cout << "\nError. Please enter a valid number 0-5.";
+                }
+
+
+
+            } while (selectColorCondition);
+            ClearInput(); 
+            std::cout << "\n"; 
+
+            RepaintCar(&arrayOfCars[carIndex], newColor); // Changes the mColor member field of the selected car object.
+
+        }
+
+
+        if (change == 2) // If the user chose to change a car's mileage.
+        {
+            // Asks the user how many miles to add to the car.
+            do
+            {
+                std::cout << "How many miles would you like to add to the car: ";
+                std::string enteredMilesByUser; 
+                std::cin >> enteredMilesByUser;
+                try
+                {
+                    addedMiles = std::stoi(enteredMilesByUser);
+                    if (addedMiles >= 0)
+                    {
+                        changeMilesCondition = false;
+                    }
+                    else
+                    {
+                        std::cout << "Cars can't have negative mileage. Please try again.\n"; 
+                    }
+                }
+                catch (...)
+                {
+                    std::cout << "Error. Please enter a valid number.\n";
+                }
+
+            } while (changeMilesCondition);
+            ClearInput();
+            std::cout << "\n";
+
+            // Adds the amount of miles chosen by the user to the mMileage member field.
+            AddMileage(&arrayOfCars[carIndex], addedMiles);
+
+        }
+
+    } while (exitProgram); 
+
+}
 
 
 
@@ -158,13 +335,11 @@ void PrintIntAndMemory(int numb, int* pointer)
     std::cout << "Value:\t" << numb << "\t\tMemory address:\t" << pointer << std::endl;
 }
 
-
 void ClearInput()
 {
     std::cin.clear(); // clears the fail FLAGS, not the buffer
     std::cin.ignore(INT_MAX, '\n'); // actually clears what was typed into the cin buffer
 }
-
 
 
 void GetCarInfo(Car& car)
@@ -187,7 +362,7 @@ void GetCarInfo(Car& car)
         try
         {
             car.mYear = std::stoi(input);
-            if (car.mYear > 1886)
+            if (car.mYear > 1886) // Checks for an appropriate year for a vehicle.
             {
                 condition1 = false;
             }
@@ -197,7 +372,7 @@ void GetCarInfo(Car& car)
             }
             
         }
-        catch (...)
+        catch (...) // If the string cannot be converted into an int.
         {
             std::cout << "Error. Please enter a valid number.\n";
         }
@@ -216,7 +391,7 @@ void GetCarInfo(Car& car)
         try
         {
             car.mMileage = std::stoi(input);
-            if (car.mMileage >= 0)
+            if (car.mMileage >= 0) // Checks if number entered is positive or equals to zero.
             {
                 condition2 = false;
             }
@@ -225,7 +400,7 @@ void GetCarInfo(Car& car)
                 std::cout << "Cars can't have negative mileage. Please try again.\n";
             }
         }
-        catch (...)
+        catch (...) // If the string cannot be converted into an int.
         {
             std::cout << "Error. Please enter a valid number.\n";
         }
@@ -240,6 +415,7 @@ void GetCarInfo(Car& car)
         std::cout << color << std::endl;
 
     }
+
     bool condition3 = true; // exit condition for do while loop
     do
     {
@@ -249,7 +425,7 @@ void GetCarInfo(Car& car)
         try
         {
             int selectedColor = std::stoi(input);
-            if (selectedColor >= 0 && selectedColor <= 5)
+            if (selectedColor >= 0 && selectedColor <= 5) // Checks if the number entered is between 0-5.
             {
                 car.mColor = (EnumColorDefinition)selectedColor; 
                 condition3 = false;
@@ -259,11 +435,10 @@ void GetCarInfo(Car& car)
                 std::cout << "\nError. Please enter a valid number 0-5.";
             }
         }
-        catch (...)
+        catch (...) // If the string cannot be converted into an int.
         {
             std::cout << "\nError. Please enter a valid number 0-5.";
         }
-
 
     } while (condition3);   
     ClearInput(); 
@@ -272,21 +447,73 @@ void GetCarInfo(Car& car)
 
 
 
+void RepaintCar(Car* ptrCar, EnumColorDefinition newColor)
+{
+    ptrCar->mColor = newColor; 
+}
+
+void AddMileage(Car* ptrCar, int milesToAdd) 
+{
+    ptrCar->mMileage += milesToAdd;
+}
 
 
 
+void PrintCar(Car car)
+{
+    std::string carColor = ShowCarColor(car.mColor);
+
+    std::cout << car.mYear << " "; 
+    std::cout << carColor << " "; 
+    std::cout << car.mMake << " "; 
+    std::cout << car.mModel << " with "; 
+    std::cout << car.mMileage << " miles";
+    std::cout << "\n\n////////////////////////\n\n";
+
+}
+
+void PrintCarPointer(Car* ptrCar) 
+{
+    PrintCar(*ptrCar);
+}
 
 
+std::string ShowCarColor(int colorIndex)
+{
+    std::string carColor; // Stores what color the car will be as a string.
+    switch (colorIndex) // will set carColor string as one of the colors based on the enum value of the mColor member field
+    {
+    case 0:
+    {
+        carColor = "Red"; 
+        break;
+    }
+    case 1:
+    {
+        carColor = "Blue"; 
+        break;
+    }
+    case 2:
+    {
+        carColor = "Silver"; 
+        break;
+    }
+    case 3:
+    {
+        carColor = "White";
+        break;
+    }
+    case 4:
+    {
+        carColor = "Black";
+        break;
+    }
+    case 5:
+    {
+        carColor = "Orange";
+        break;
+    }
+    }
+    return carColor;
+}
 
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
