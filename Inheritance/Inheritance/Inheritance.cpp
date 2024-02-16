8caeb420296a6fa113b5fdd5758d5356085cfcb8
Kennan Gauthier <KDGauthier@student.fullsail.edu>
2024-02-16 00:46:41 -0500

    Beginning the implementation of UI functionality.

--- Inheritance/Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance/Inheritance.cpp
@@ -7,6 +7,12 @@
 #include "SavingsAccount.h"
 #include "CreditAccount.h"
 
+void Withdraw(BaseAccount* accountPtr, float amount); // Will call the correct Class's Withdraw() function
+
+
+void Deposit(BaseAccount* accountPtr, float amount); // Will call the correct Class's Deposit() function
+
+
 
 int main()
 {
@@ -14,22 +20,22 @@ int main()
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     _CrtSetBreakAlloc(-1); // set block of memory to find memory leak
     _CrtDumpMemoryLeaks();
-    
-    CreditAccount test;
-    test.Withdraw(30);
-    std::cout << "\n$30 withdrawn:\t" << "Data: " << test.GetData() << "\tAccount Balance: " << test.GetBalance() << "\n\n";
-    test.Withdraw(20);
-    std::cout << "Data: " << test.GetData() << std::endl;
-    test.Withdraw(1);
-    std::cout << "Data: " << test.GetData() << std::endl;
-    test.Deposit(39);
-    std::cout << "\n$39 deposited:\t" << "Data: " << test.GetData() << "\tAccount Balance: " << test.GetBalance() << "\n\n";
-    test.Deposit(15);
-    std::cout << "\n$15 deposited:\t" << "Data: " << test.GetData() << "\t\tAccount Balance: " << test.GetBalance() << "\n\n";
-    test.Deposit(4985);
-    std::cout << "\n$4985 deposited:\t" << "Data: " << test.GetData() << "\t\tAccount Balance: " << test.GetBalance() << "\n\n";
+    /*bool exitProgram = true;
+    do
+    {
+
+    } while (exitProgram);*/
 
 
 }
 
+void Withdraw(BaseAccount* accountPtr, float amount)
+{
+    accountPtr->Withdraw(amount);
+}
 
+
+void Deposit(BaseAccount* accountPtr, float amount)
+{
+    accountPtr->Deposit(amount);
+}
